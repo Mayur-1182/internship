b07a9c2809735b69fed4a350f6fef5801fa4cc2f
MAYUR PARMAR <parmar.mayurumb11@gmail.com>
2024-01-10 11:46:00 +0530

    .

--- src/component/RegistrationPage.jsx
+++ src/component/RegistrationPage.jsx
@@ -1,45 +1,69 @@
 import React, { Fragment, useState } from "react";
+import { signInSchema } from "../schemas/RegistrationShema";
 import "./Registration.css";
+import { useFormik } from "formik";
+
+const initialValues = {
+  user_name: "",
+  first_name: "",
+  last_name: "",
+  email: "",
+  password: "",
+  designation: "",
+  company_name: "",
+  phoneNumber: "",
+};
 
 export default function RegistrationPage() {
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: initialValues,
+      validationSchema: signInSchema,
+      onSubmit: (values, action) => {
+        console.log("Form Submitted");
+        console.log(values);
+        action.resetForm();
+      },
+    });
+  console.log(errors);
   return (
     <Fragment>
       <h1>Registration Form</h1>
-      <form className="registration_form">
+      <form className="registration_form" onSubmit={handleSubmit}>
         <Fragment>
           <div className="user_controls">
             <label htmlFor="user_name">Username</label>
-            <input type="text" id="user_name" name="user_name" />
+            <input type="text" id="user_name" name="user_name" onChange={handleChange} onBlur={handleBlur} value={values.user_name}/>
           </div>
           <div className="user_controls">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" />
+            <input type="password" id="password" name="password" onChange={handleChange} onBlur={handleBlur} value={values.password}/>
           </div>
           <div className="user_controls">
             <label htmlFor="first_name">First Name</label>
-            <input type="text" id="first_name" name="firstName" />
+            <input type="text" id="first_name" name="first_name" onChange={handleChange} onBlur={handleBlur} value={values.first_name}/>
           </div>
           <div className="user_controls">
             <label htmlFor="last_name">Last Name</label>
-            <input type="text" id="last_name" name="last_name" />
+            <input type="text" id="last_name" name="last_name" onChange={handleChange} onBlur={handleBlur} value={values.last_name}/>
           </div>
           <div className="user_controls">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" />
+            <input type="email" id="email" name="email"  onChange={handleChange} onBlur={handleBlur} value={values.email}/>
           </div>
           <div className="user_controls">
             <label htmlFor="phone">Phone Number</label>
-            <input type="tel" id="phone" name="phoneNumber" />
+            <input type="tel" id="phone" name="phoneNumber" onChange={handleChange} onBlur={handleBlur} value={values.phoneNumber} />
           </div>
         </Fragment>
         <Fragment>
           <div className="user_controls">
             <label htmlFor="company_name">Company Name</label>
-            <input type="text" id="company_name" name="company_name" />
+            <input type="text" id="company_name" name="company_name"  onChange={handleChange} onBlur={handleBlur} value={values.company_name}/>
           </div>
           <div className="user_controls">
             <label htmlFor="designation">Designation</label>
-            <input type="text" id="designation" name="designation" />
+            <input type="text" id="designation" name="designation"  onChange={handleChange} onBlur={handleBlur} value={values.designation}/>
           </div>
           <div className="user_actions">
             <button type="submit">SignUp</button>
--- src/schemas/RegistrationShema.jsx
+++ src/schemas/RegistrationShema.jsx
@@ -1,12 +1,28 @@
 import * as Yup from "yup";
 
 export const signInSchema = Yup.object({
-  fullName: Yup.string()
-    .min(4, "Enter Valid Name")
-    .max(30, "Name is Too larger")
-    .required("Name is Required Field"),
+  // fullName: Yup.string()
+  //   .min(4, "Enter Valid Name")
+  //   .max(30, "Name is Too larger")
+  //   .required("Name is Required Field"),
+  // email: Yup.string()
+  //   .email("Invalid email address")
+  //   .required("email is required filed")
+  //   .test({
+  //     name: "email validation",
+  //     message: "entered email address is invalid ",
+  //     test: (email) => {
+  //       const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  //       return emailRegex.test(email);
+  //     },
+  //   }),
+  // phone: Yup.string().min(10).max(12).required("Phone number is required!!"),
+  // password: Yup.string()
+  //   .min(6, "Password is too small")
+  //   .required("Password is required filed!!"),
+  user_name: Yup.string().min(2).max(15).required("Username is required!"),
   email: Yup.string()
-    .email("Invalid email adress")
+    .email("Invalid email address")
     .required("email is required filed")
     .test({
       name: "email validation",
@@ -16,8 +32,14 @@ export const signInSchema = Yup.object({
         return emailRegex.test(email);
       },
     }),
-  phone: Yup.string().min(10).max(12).required("Phone number is required!!"),
   password: Yup.string()
     .min(6, "Password is too small")
     .required("Password is required filed!!"),
+  first_name: Yup.string().min(2).max(15).required("FirstName is required!"),
+  last_name: Yup.string().min(2).max(15).required("LastName is required!"),
+  company_name: Yup.string()
+    .min(2)
+    .max(15)
+    .required("Company Name is required!"),
+  designation: Yup.string().min(2).max(15).required("designation is required!"),
 });
